e9d889ea3e78f0fe330a09a314ee7617bdbe0140
alejandrodgz <5199@holbertonstudents.com>
2022-12-13 16:13:25 -0500

    task0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,38 @@
-
 #!/usr/bin/python3
 """
-Module 0-gather_data_from_an_API
+This module containts an api request
 """
+import requests
+import sys
+
+
+def gather_data_from_api():
+    """
+        This function gather data from an api
+    """
+
+    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId='
+    url_name = 'https://jsonplaceholder.typicode.com/users?id='
+    response_todo = requests.get(url_todo + sys.argv[1])
+    response_name = requests.get(url_name + sys.argv[1])
+
+    content_todo = list(response_todo.json())
+    content_name = list(response_name.json())
+
+    task_completed = 0
+    total_tasks = 0
+    completed_tasks = []
+    for elem in content_todo:
+        if elem['completed']:
+            task_completed += 1
+            completed_tasks.append(elem['title'])
+        total_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        content_name[0]['name'], task_completed, total_tasks))
+    for elem in completed_tasks:
+        print('\t ' + elem)
+
+
 if __name__ == "__main__":
-    import requests
-    import sys
-
-    user_id = sys.argv[1]
-    url_employee = "https://jsonplaceholder.typicode.com/users/{}"\
-        .format(user_id)
-    url_todo = "https://jsonplaceholder.typicode.com/todos?userId={}"\
-        .format(user_id)
-    req_employee = requests.get(url_employee)
-    EN = req_employee.json().get('name')
-    req_todo = requests.get(url_todo)
-    TOTAL_T = len(req_todo.json())
-    TASKS = 0
-    NUMT = 0
-    lists = []
-    while TASKS < TOTAL_T:
-        if req_todo.json()[TASKS].get('completed') is True:
-            lists.append(req_todo.json()[TASKS].get('title'))
-            NUMT += 1
-        TASKS += 1
-
-    print("Employee {} is done with tasks({}/{}):".format(EN, NUMT, TOTAL_T))
-    for t in lists:
-        print("\t {}".format(t))
+    gather_data_from_api()
