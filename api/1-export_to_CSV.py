fbd5afb4ab311e9b4227d3ecc2e6cc4599963fff
alejandrodgz <5199@holbertonstudents.com>
2022-12-13 17:47:34 -0500

    task1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''api to convert to csv'''
+
+import csv
+import requests
+import sys
+
+
+def api_get_name():
+    """function to make the format and print the info"""
+
+    EMPLOYEE_NAME = ""
+    id_user = int(sys.argv[1])
+    users = "https://jsonplaceholder.typicode.com/users"
+    todos = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id_user)
+    response = requests.get(users).json()
+
+    for i in response:
+        if i["id"] == id_user:
+            EMPLOYEE_NAME = i['username']
+
+    TASK_CSV = []
+    response1 = requests.get(todos).json()
+    for j in response1:
+        listing = [str(j["userId"]), EMPLOYEE_NAME, j["completed"], j['title']]
+        TASK_CSV.append(listing)
+    
+    namefile = "{}.csv".format(id_user)
+    with open(namefile, "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(TASK_CSV)
+
+
+if __name__ == '__main__':
+    api_get_name()
