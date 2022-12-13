29249612b550ff29347c7376995b24d4a3b7c183
alejandrodgz <5199@holbertonstudents.com>
2022-12-13 15:29:16 -0500

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''gather information from the an api'''
 
-import json
 import requests
 import sys
 
@@ -9,26 +8,22 @@ import sys
 def api_get_name():
     """function to make the format and print the info"""
 
-    id_user = sys.argv[1]
+    id_user = int(sys.argv[1])
     users = "https://jsonplaceholder.typicode.com/users/"
     url = "{}{}".format(users, id_user)
-    response = requests.get(url)
-    get1 = response.content
-    EMPLOYEE_NAME = json.loads(get1)['name']
-    TASK_TITLE =[]
-
+    response = requests.get(url).json()
+    EMPLOYEE_NAME = response['name']
+    TASK_TITLE = []
 
     url1 = "{}{}/todos".format(users, id_user)
-    response = requests.get(url1)
-    dict_get1 = response.json()
+    response1 = requests.get(url1).json()
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
-    for elem in dict_get1:
+    for elem in response1:
         TOTAL_NUMBER_OF_TASKS += 1
         if elem["completed"]:
             NUMBER_OF_DONE_TASKS += 1
             TASK_TITLE.append(elem['title'])
-
 
     print(
         "Employee {} is done with tasks({}/{}):"
